387cf8538e57e82a73c7dd1919cd5c7ccc79a605
Florian Zinggeler <florian.zinggeler@simplificator.com>
2019-02-07 16:53:26 +0100

    Clean up index

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
 import Peer from "peerjs";
 import { Elm } from "./dist/elm.js";
 
-// const base_id = "ZGe07Cx9SJinbtV5tw8JN++EBwHPC7Z8n0youC5v8pA=";
-// const my_id = "" + Math.round(Math.random() * 10);
-
 const app = Elm.Main.init({
   node: document.getElementById("main")
-  // flags: my_id
 });
-
-// console.log("my id:", my_id);
 
 const peer = new Peer({ debug: 2 });
 
-// debugger;
-// const conn = peer.connect(base_id + "3");
-// conn.on("open", function() {
-//   conn.send("hi!");
-// });
-
 peer.on("open", id => {
-  app.ports.input.send({ type: "GotId", id });
   console.log("open", id);
+  app.ports.input.send({ type: "GotId", id });
 });
+
 peer.on("connection", conn => {
   console.log("connection", conn);
   window.conn = conn;
@@ -30,20 +19,8 @@ peer.on("connection", conn => {
     console.log("Received", data);
     app.ports.input.send({ type: "GotMsg", data });
   });
-  // Got paired
   setTimeout(() => conn.send({ type: "Ready" }), 1000);
 });
-peer.on("disconnected", conn => {
-  console.log("disconnected", conn);
-});
-peer.on("close", conn => {
-  console.log("close", conn);
-});
-peer.on("error", error => {
-  console.log("error", error);
-});
-
-// let conn = null;
 
 app.ports.output.subscribe(data => {
   console.log("elm data", data);
@@ -59,4 +36,16 @@ app.ports.output.subscribe(data => {
     window.conn.send(data.data);
   }
   // app.ports.input.send(42);
+});
+
+peer.on("disconnected", conn => {
+  console.log("disconnected", conn);
+});
+
+peer.on("close", conn => {
+  console.log("close", conn);
+});
+
+peer.on("error", error => {
+  console.log("error", error);
 });
